6fc24bde329b276eb6d737fd178ba83f8ed633ac
Hyago Eurico <hyago.eurico.dev@gmail.com>
2024-04-16 21:36:57 -0300

    feat: added 'get' methods

--- src/main/java/com/example/demoCRUD/controllers/DemoController.java
+++ src/main/java/com/example/demoCRUD/controllers/DemoController.java
@@ -9,9 +9,12 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 public class DemoController {
@@ -24,5 +27,19 @@ public class DemoController {
         var demoModel = new DemoModel();
         BeanUtils.copyProperties(demoRecordDto, demoModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(demoReporitory.save(demoModel));
+    }
+
+    @GetMapping("/products")
+    public ResponseEntity<List<DemoModel>> getAllProducts(){
+        return ResponseEntity.ok(demoReporitory.findAll());
+    }
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Object> getProductById(@PathVariable(value = "id") UUID id){
+        Optional<DemoModel> productO = demoReporitory.findById(id);
+        if(productO.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+        }
+        return ResponseEntity.ok(productO.get());
     }
 }
